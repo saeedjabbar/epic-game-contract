e2274864a571598d4b9d1f38337530c81d77171d
Saeed Jabbar <saeedryanjabbar@gmail.com>
2021-10-29 23:38:01 -0400

    contract updated

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,26 +7,16 @@ const main = async () => {
     ["https://i.imgur.com/Q90mY2o.gif", // Images
       "https://i.imgur.com/ycKfK3x.gif",
       "https://i.imgur.com/jwub4R5.gif"],
-    [100, 200, 300],                    // HP values
-    [100, 50, 25],                       // Attack damage values
+    [100, 100, 100],                    // HP values
+    [3, 2, 4],                       // Attack damage values
     "Bob Sponge", // Boss name
     "https://i.imgur.com/XqQ2Wps.gif", // Boss image
-    10000, // Boss hp
-    50 // Boss attack damage
+    100, // Boss hp
+    3 // Boss attack damage
   );
     //mine and deploy contract
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
-
-    let txn;
-    txn = await gameContract.mintCharacterNFT(2);
-    await txn.wait();
-
-    txn = await gameContract.attackBoss();
-    await txn.wait();
-
-    txn = await gameContract.attackBoss();
-    await txn.wait();
 
     // Get the value of the NFT's URI. // meta deta
     // let returnedTokenUri = await gameContract.tokenURI(1);
